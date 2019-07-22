cfc8df318011a0c81b02c2785e4dcf6f1319d6eb
R. van Veen <rvanveen@servoy.com>
2019-07-22 09:58:17 +0200

    Moved sales-demo component out of servoyextra

---
+++ customrenderedcomponents/daynavigator/daynavigator.js
@@ -0,0 +1,52 @@
+angular.module('customrenderedcomponentsDaynavigator', ['servoy']).directive('customrenderedcomponentsDaynavigator', ['$log', '$timeout',
+function($log, $timeout) {
+    return {
+        restrict: 'E',
+        scope: {
+            model: '=svyModel',
+            handlers: '=svyHandlers',
+            api: '=svyApi',
+            svyServoyapi: '='
+        },
+        link: function($scope, $element, $attrs) {
+            var datepaginatorElement = null;
+            var selectedMonth = '';
+            $timeout(function() {
+                datepaginatorElement = $element.find('.svy-extra-daynavigator');
+                datepaginatorElement.datepaginator({
+                    showCalendar: false,
+                    onSelectedDateChanged: function(event, date) {
+                        selectedMonth = date.format('MMM')
+                        if($scope.handlers.onChange) {
+                            $scope.handlers.onChange(date);
+                        }
+                    },
+                    selectedDate: selectedDateToMomentDate(),
+					// TODO expose this properties to the model ?
+					text: "DD",
+					textSelected: "DD",
+					width: 240,
+					selectedItemWidth: 40,
+					itemWidth: 40
+                });
+            });
+
+            $scope.$watch("model.selectedDate", function(newValue, oldValue) {
+                var momentDate = selectedDateToMomentDate();
+                selectedMonth = momentDate.format('MMM');
+                if(datepaginatorElement) {
+                    datepaginatorElement.datepaginator('setSelectedDate', momentDate);
+                }
+            });
+
+            $scope.getMonth = function() {
+                return selectedMonth;
+            }
+
+            function selectedDateToMomentDate() {
+                return $scope.model.selectedDate ? moment($scope.model.selectedDate) : moment();
+            }
+        },
+        templateUrl: 'customrenderedcomponents/daynavigator/daynavigator.html'
+    }
+}]);
---
+++ customrenderedcomponents/daynavigator/daynavigator.spec
@@ -0,0 +1,26 @@
+{
+	"name": "customrenderedcomponents-daynavigator",
+	"displayName": "Day navigator",
+	"version": 1,
+	"definition": "customrenderedcomponents/daynavigator/daynavigator.js",
+    "libraries": [{"name":"moment", "version":"2.11.1", "url": "customrenderedcomponents/daynavigator/moment-with-locales.min.js", "mimetype":"text/javascript"},
+                  {"name":"moment-jdateformatparser", "version":"0.1.1", "url":"customrenderedcomponents/daynavigator/moment-jdateformatparser.js", "mimetype":"text/javascript"},
+                  { "name": "daynavigator.css", "version": "1.0", "url": "customrenderedcomponents/daynavigator/daynavigator.css", "mimetype": "text/css" }, 
+    			  {"name":"bootstrap-datepaginator.css", "version":"1", "url":"customrenderedcomponents/daynavigator/lib/bootstrap-datepaginator.css", "mimetype":"text/css"}, 
+    			  {"name":"bootstrap-datepaginator.js", "version":"1", "url":"customrenderedcomponents/daynavigator/lib/bootstrap-datepaginator.js", "mimetype":"text/javascript"}],
+	"model":
+	{
+		"selectedDate": "date",
+		"visible": "visible"
+	},
+	"handlers" :
+	{
+		"onChange": {
+			"description": "Called when the day is changed",
+			"parameters": [{
+				"name": "date",
+				"type": "date"
+			}]
+		}
+	}
+}
